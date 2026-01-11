2d6b24a0fc4063897742f1892836246fbf2abc4e
Zeinab <zeinabsadjadi@gmail.com>
2026-01-11 23:56:20 +0100

    styling cards

--- PersonalDiary/src/App.jsx
+++ PersonalDiary/src/App.jsx
@@ -37,7 +37,7 @@ function App() {
           <AddEntryModal onClose={CloseModal} onSave={handleSave} />{" "}
         </div>
       )}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 m-4 ">
+      <div className=" m-8  grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-4 gap-y-6 justify-center">
         {entries.map((entry) => (
           <DiaryCard
             key={entry.id}
