99557830f235b8f5da11fe1873ed38ca702576b0
Zeinab <zeinabsadjadi@gmail.com>
2026-01-12 13:27:59 +0100

    finalized project as instruction, no dlete edit yet

--- PersonalDiary/src/App.jsx
+++ PersonalDiary/src/App.jsx
@@ -31,6 +31,7 @@ function App() {
 
     const entryWithId = { ...newEntry, id: Date.now() };
     setEntries((prev) => [...prev, entryWithId]);
+    console.log("Salam", entryWithId.id);
   };
 
   useEffect(() => {
@@ -51,16 +52,18 @@ function App() {
         </div>
       )}
       <div className=" m-6 pb-6 grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-4 gap-y-6 justify-center">
-        {entries.map((entry) => (
-          <DiaryCard
-            key={entry.id}
-            onClickCard={() => OpenCard(entry)}
-            title={entry.title}
-            date={entry.date}
-            image={entry.url}
-            content={entry.content}
-          />
-        ))}
+        {[...entries]
+          .sort((a, b) => new Date(b.date) - new Date(a.date))
+          .map((entry) => (
+            <DiaryCard
+              key={entry.id}
+              onClickCard={() => OpenCard(entry)}
+              title={entry.title}
+              date={entry.date}
+              image={entry.url}
+              content={entry.content}
+            />
+          ))}
       </div>
 
       {selectedCard && (
